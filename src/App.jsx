0723486e0b88f7ba9c01f7f7ce0f52fece86fa68
thevictor <victor.d.alexa@gmail.com>
2024-11-25 19:29:17 +0200

    pdf render wip (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import PDFFile from "./components/PDFFile";
-import { PDFDownloadLink } from "@react-pdf/renderer";
+// import { PDFDownloadLink } from "@react-pdf/renderer";
+
+import ReactPDF, {
+  Page,
+  Text,
+  Image,
+  Document,
+  StyleSheet,
+  PDFViewer,
+} from "@react-pdf/renderer";
 
 function App() {
+  const [client, setClient] = useState(false);
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
   return (
     <div className="App">
-      <PDFDownloadLink document={<PDFFile />} fileName="TEST PDF">
+      {/* <PDFDownloadLink document={<PDFFile />} fileName="TEST PDF">
         {({ loading }) =>
           loading ? (
             <button>loading document...</button>
@@ -12,7 +27,7 @@ function App() {
             <button>Download</button>
           )
         }
-      </PDFDownloadLink>
+      </PDFDownloadLink> */}
       <PDFFile />
     </div>
   );
