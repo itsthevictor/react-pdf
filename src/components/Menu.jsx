ac22bf1a9ee3653871838d14a5326428a3695888
thevictor <victor.d.alexa@gmail.com>
2024-11-29 08:05:42 +0200

    send pdf as blob

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,27 @@
-import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PDFDownloadLink, BlobProvider, pdf } from "@react-pdf/renderer";
 import PDF from "./PDF";
+import axios from "axios";
 const Menu = ({ view, setView, poem }) => {
+  const handleSendPdf = async () => {
+    const blob = await pdf(<PDF poem={poem} />).toBlob();
+    // console.log(blob);
+
+    // Send the Blob to the server
+    const formData = new FormData();
+    formData.append("pdfFile", blob, "my-pdf.pdf");
+    console.log(Object.fromEntries(formData));
+
+    // ... (Send the FormData to the server)
+  };
   return (
     <nav>
       <button onClick={() => setView("web")}>Ver Web</button>
-
       <button onClick={() => setView("pdf")}>Ver PDF</button>
-
       <PDFDownloadLink document={<PDF poem={poem} />} fileName="el test.pdf">
         <button>Descargar</button>
       </PDFDownloadLink>
+
+      <button onClick={handleSendPdf}>make blob</button>
     </nav>
   );
 };
